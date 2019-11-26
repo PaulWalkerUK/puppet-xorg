08cef20c089413b70f0ad38f04988ec7b94cfc55
PaulWalkerUK <walker.paul+github@gmail.com>
2019-11-26 00:30:17 +0000

    Add parameters so optional packages are... optional!

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,43 @@
 # @summary This class manages xorg
 #
+# @param xorg_server_manage
+#   Whether to manage to xorg_server package. Default calue: true.
+#
+# @param xorg_server_ensure
+#   If $xorg_server_manage is `true`, then whether to install the `xorg_server` 
+#   package, and what version to install. Values: 'present', 'latest', or a 
+#   specific version number. Default value: 'present'.
+#
+# @param xorg_apps_manage
+#   Whether to manage to xorg_apps package. Default calue: true.
+#
+# @param xorg_apps_ensure
+#   If $xorg_apps_manage is `true`, then whether to install the `xorg_apps` 
+#   package, and what version to install. Values: 'present', 'latest', or a 
+#   specific version number. Default value: 'present'.
+#
+# @param xorg_init_manage
+#   Whether to manage to xorg_init package. Default calue: true.
+#
+# @param xorg_init_ensure
+#   If $xorg_init_manage is `true`, then whether to install the `xorg_init` 
+#   package, and what version to install. Values: 'present', 'latest', or a 
+#   specific version number. Default value: 'present'.
+#
+# @param xorg_xterm_manage
+#   Whether to manage to xorg_xterm package. Default calue: true.
+#
+# @param xorg_xterm_ensure
+#   If $xorg_xterm_manage is `true`, then whether to install the `xorg_xterm` 
+#   package, and what version to install. Values: 'present', 'latest', or a 
+#   specific version number. Default value: 'present'.
+#
 # @example
 #   include xorg
-class xorg {
+class xorg (
+  String $xorg_server_manage,
+  Boolean $xorg_server_ensure,
+){
   contain xorg::install
   contain xorg::config
   contain xorg::service
--- manifests/install.pp
+++ manifests/install.pp
@@ -11,9 +11,20 @@ class xorg::install {
   package { 'virtualbox-guest-modules-arch': ensure => 'present' }
   package { 'mesa': ensure => 'present' }
 
-  # Addons
-  package { 'xorg-server': ensure => 'present' }
-  package { 'xorg-apps': ensure => 'present' }
-  package { 'xorg-xinit': ensure => 'present' }
-  package { 'xterm': ensure => 'present' }
+  # Optional addons
+  if $xorg::xorg_server_manage {
+    package { 'xorg-server': ensure => $xorg::xorg_server_ensure }
+  }
+
+  if $xorg::xorg_apps_manage {
+    package { 'xorg-apps': ensure => $xorg::xorg_apps_ensure }
+  }
+  
+  if $xorg::xorg_init_manage {
+    package { 'xorg-xinit': ensure => $xorg::xorg_init_ensure }
+  }
+  
+  if $xorg::xterm_manage {
+    package { 'xterm': ensure => $xorg::xterm_ensure }
+  }
 }
