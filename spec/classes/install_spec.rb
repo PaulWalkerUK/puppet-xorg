46d830392150747cb9b6a46813296105b511283a
PaulWalkerUK <walker.paul+github@gmail.com>
2019-12-21 23:53:02 +0000

    Bring tests inline with changes

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -10,14 +10,7 @@ describe 'xorg::install' do
       it { is_expected.to contain_package('virtualbox-guest-utils-nox').with(ensure: 'absent') }
       it { is_expected.to contain_package('virtualbox-guest-utils').with(ensure: 'present') }
 
-      it { is_expected.to contain_package('xorg').with(ensure: 'present') }
-      it { is_expected.to contain_package('virtualbox-guest-modules-arch').with(ensure: 'present') }
-      it { is_expected.to contain_package('mesa').with(ensure: 'present') }
-
       it { is_expected.to contain_package('xorg-server').with(ensure: 'present') }
-      it { is_expected.to contain_package('xorg-apps').with(ensure: 'present') }
-      it { is_expected.to contain_package('xorg-xinit').with(ensure: 'present') }
-      it { is_expected.to contain_package('xterm').with(ensure: 'present') }
     end
   end
 end
